6bc044ba138e134605cf684cbca1d15873b2af16
Noah Cohn <noahc42@gmail.com>
2025-12-16 04:55:37 -0800

    Update VSTPlayhead.h

--- Source/VSTPlayhead.h
+++ Source/VSTPlayhead.h
@@ -33,5 +33,5 @@
 class VSTPlayhead : public juce::AudioPlayHead
 {
 public:
-std::optional<juce::AudioPlayHead::PositionInfo> getPosition() const override;
+bool getCurrentPosition(juce::AudioPlayHead::CurrentPositionInfo& result) override;
 };
